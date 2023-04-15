c0ac95f956341715af580cfce80aabbd10cea455
BogKowit <BogKowit@gmail.com>
2023-04-15 21:25:02 +0200

    app structure

---
+++ vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import './styles.css'
+</script>
+
+<template>
+  <header class="header bg-red-500">
+    <div class="bg-red-200">test 1</div>
+    <HelloWorld />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>test 2</div>
+</template>
+
+<style scoped></style>
